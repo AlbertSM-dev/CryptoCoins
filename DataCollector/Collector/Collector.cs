9e4c42b507c7a757b76e3ecc47765fce70e0231c
AlbertSM-dev <lokdok1988@hotmail.com>
2023-10-22 21:29:15 +0100

    Add project files.

---
+++ DataCollector/Collector/Collector.cs
@@ -0,0 +1,47 @@
+﻿using DataCollector.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DataCollector.Collector
+{
+    public class Collector
+    {
+        public Collector() {
+            List<Coin> Items = new List<Coin>();
+        }
+
+        public static async Task<Coin> GetCoinsAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("https://api.coincap.io/v2/assets");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    Coins obj = (Coins)JsonConvert.DeserializeObject<Coins>(data);
+                }
+                return null;
+            }
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    IEnumerable<Coin> categories = await response.Content.ReadAsAsync<IEnumerable<Coin>>();
+            //    var ItemData = new Items
+            //    (
+            //    // Use this category list to initialize your Items
+            //    );
+            //    items.Add(ItemData);
+            //}
+        }
+    }
+}
---
+++ DataCollector/Program.cs
@@ -0,0 +1,86 @@
+﻿using DataCollector.Collector;
+using DataCollector.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CodeFirstNewDatabaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new CoinContext())
+            {
+                Console.Write("Wait for the coins: ");
+                Collector.GetCoinsAsync().Wait();
+                // Create and save a new Blog
+                Console.Write("Enter a name for a new Coin: ");
+                var id = Console.ReadLine();
+                Console.Write("Enter a price for a new Coin: ");
+                var price = Console.ReadLine();
+
+                var coin = new Coin { id = id, priceUsd = price };
+                db.Coins.Add(coin);
+                db.SaveChanges();
+
+                // Display all Blogs from the database
+                var query = from b in db.Coins
+                            orderby b.id
+                            select b;
+
+                Console.WriteLine("All COINS in the database:");
+                foreach (var item in query)
+                {
+                    Console.WriteLine(item.id,":", item.priceUsd);
+                }
+
+               
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; }
+    }
+
+    public class User
+    {
+        [Key]
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+    }
+
+    public class CoinContext : DbContext
+    {
+        public DbSet<Coin> Coins { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.DisplayName)
+                .HasColumnName("display_name");
+        }
+    }
+}
